c045abb978fa70a2d3fbff40db62c10ca9f63e75
vinsmoke <bharathkaranth07@gmail.com>
2020-02-12 09:17:53 +0530

    Express setup and initial route

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+// Connect DB
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.json({ msg: 'Welcome to contact keeper API' });
+});
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+})
