bec4bf0b9b6f4c27ddea48f5abebd3038c551087
vinsmoke <bharathkaranth07@gmail.com>
2020-02-14 16:37:26 +0530

    Add React Transition animations to Contact component

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ContactContext from '../../context/contact/contactContext';
 import ContactItem from './ContactItem';
 
@@ -13,15 +14,21 @@ const Contacts = () => {
     return (
         <Fragment>
             <div className="container-fluid">
-                {
-                    filtered !== null
-                        ? filtered.map(contact =>
-                            <ContactItem key={contact.id} contact={contact} />)
-                        : contacts.map(contact =>
-                            <ContactItem key={contact.id} contact={contact} />)
-                }
+                <TransitionGroup>
+                    {
+                        filtered !== null
+                            ? filtered.map(contact =>
+                                <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                                    <ContactItem contact={contact} />
+                                </CSSTransition>
+                            )
+                            : contacts.map(contact =>
+                                <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                                    <ContactItem key={contact.id} contact={contact} />
+                                </CSSTransition>)
+                    }
+                </TransitionGroup>
             </div>
-
         </Fragment>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
