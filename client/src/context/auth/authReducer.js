8e007c721de7de8d162ac1eda55c9f2e148ce3e9
vinsmoke <bharathkaranth07@gmail.com>
2020-02-14 18:21:09 +0530

    Add auth Context and Initial State

---
+++ client/src/context/auth/authReducer.js
@@ -0,0 +1,13 @@
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGOUT,
+    CLEAR_ERRORS,
+} from '../types';
+
+export default (state, action) => {
+    const { type, payload } = action;
+}
