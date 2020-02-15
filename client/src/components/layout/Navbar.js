18bbd97bfbd3b170b30e88d2e06e6cb47ebd97a2
vinsmoke <bharathkaranth07@gmail.com>
2020-02-15 13:07:05 +0530

    Add guestLinks and authLinks to Navbar component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
 
 const Navbar = ({ title, icon }) => {
+    const authContext = useContext(AuthContext);
+
+    const { isAuthenticated, logout, user } = authContext;
+
+    const onLogout = () => {
+        logout();
+    }
+
+    const authLinks = (
+        <Fragment>
+            <li style={{ marginLeft: "10px" }} className="nav-item">
+                <button className="btn btn-light" active>Hello {user && user.name}</button>
+            </li>
+            <li style={{ marginLeft: "10px" }} className="nav-item">
+                <button onClick={onLogout} className="btn btn-outline-light"><i className="fas fa-sign-out-alt mr-1"></i>Logout</button>
+            </li>
+        </Fragment>
+    )
+    const guestLinks = (
+        <Fragment>
+            <li style={{ marginLeft: "10px" }} className="nav-item">
+                <Link to="/register">
+                    <button className="btn btn-outline-light">Register</button>
+                </Link>
+            </li>
+            <li style={{ marginLeft: "10px" }} className="nav-item">
+                <Link to="/login">
+                    <button className="btn btn-outline-light">Login</button>
+                </Link>
+            </li>
+            <li style={{ marginLeft: "10px" }} className="nav-item">
+                <Link to="/about">
+                    <button className="btn btn-outline-light">About</button>
+                </Link>
+            </li>
+        </Fragment>
+    )
     return (
         <nav className="navbar navbar-dark bg-dark">
             <Link className="navbar-brand" to="/">
@@ -10,26 +48,7 @@ const Navbar = ({ title, icon }) => {
                 {' ' + title}
             </Link>
             <ul className="nav">
-                <li className="nav-item">
-                    <Link to="/">
-                        <button className="btn btn-outline-light">Home</button>
-                    </Link>
-                </li>
-                <li style={{ marginLeft: "10px" }} className="nav-item">
-                    <Link to="/about">
-                        <button className="btn btn-outline-light">About</button>
-                    </Link>
-                </li>
-                <li style={{ marginLeft: "10px" }} className="nav-item">
-                    <Link to="/register">
-                        <button className="btn btn-outline-light">Register</button>
-                    </Link>
-                </li>
-                <li style={{ marginLeft: "10px" }} className="nav-item">
-                    <Link to="/login">
-                        <button className="btn btn-outline-light">Login</button>
-                    </Link>
-                </li>
+                {isAuthenticated ? authLinks : guestLinks}
             </ul>
         </nav>
     )
